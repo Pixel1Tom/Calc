c29b3ed193d174c99b2a14f8ad21bb5d950dceda
Pixel1Tom <Pixel1Tom@users.noreply.github.com>
2020-06-23 14:31:42 +0300

    Adding the project files

---
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+enum class CalcMode {
+    None, Addition, Substraction, Multiplication
+}
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val mode = CalcMode.Substraction
+        tw_one.text = "text one $mode"
+
+    }
+}
